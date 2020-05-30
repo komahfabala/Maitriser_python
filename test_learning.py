850eb3e7280c08fd6c0e8e54413135aecb95cdcb
komahfabala <komahfabala@gmail.com>
2020-05-30 11:05:38 +0200

    manipulation de numpy

--- test_learning.py
+++ test_learning.py
@@ -1,3 +1,25 @@
-import numpy as nps
+import numpy as np
 
 #----- mone premier -------
+A = np.arange(4)
+print('A =', A)
+
+B = np.arange(12).reshape(2, 6)
+print('B =', B)
+print("\n")
+
+def initialisation(m,n) :
+    """
+        m nombre de colonne de la matrice
+        n nombre de ligne de la matrice
+        retourne une matrice aleatoire (m,n+1)
+        #avec une colonne de biais(remplie de 1) tout a droite
+    """
+    mat_aleatoire= np.random.randn(m,n+1)
+    mat_biais = np.ones((m,1))
+    np.random.seed(0)
+    mat_complet =np.concatenate((mat_aleatoire,mat_biais),axis=1)
+    return mat_complet
+if __name__ == "__main__":
+    tes= initialisation(5,4)
+    print(tes)
