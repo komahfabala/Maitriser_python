e5b71278092bd6230b1bcfd9109e571e5b13c06e
komahfabala <komahfabala@gmail.com>
2020-06-01 22:33:21 +0200

    utilisation de la fonction unique , argsort de numpy

--- indexingSlicingSubseting.py
+++ indexingSlicingSubseting.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 random.seed(2)
 
-suduku = np.ones((9,9))
+suduku = np.random.randint(0,10,[9,9])
 suduku[0:3,0:3] = 10
 suduku[0:6,3:6] = 4
 suduku[0:3,6:9] =3
@@ -12,3 +12,9 @@ suduku[3:6,3:6] = 9
 suduku[6:9,0:3] = 7
 suduku[6:9,6:9] = 2
 print(suduku)
+carre1=suduku[0:3,0:3] 
+print(carre1)
+valeurs , counts = np.unique(suduku,return_counts=True) #compte le nombre de repetition
+for i,j in zip(valeurs[counts.argsort()],counts[counts.argsort()]): #tri du tableau des en fonction des occurences
+    print("valeurs {} apparrait {}".format(i,j) )
+
