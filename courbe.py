f2722bae5497529477565efc9ebdc0f081a7ff33
komahfabala <komahfabala@gmail.com>
2020-06-03 22:38:05 +0200

    tracer 4 graphiques sur la meme figure

---
+++ courbe.py
@@ -0,0 +1,17 @@
+from numpy import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataSet = {f"experinece{i}" : random.randn(100) for i in range(5)} #nestead dict
+
+def graphique(dataSet_val) :
+    plt.figure(figsize=(12,8))
+    for k,i in zip(dataSet_val.keys(), range(1,len(dataSet_val)+1)):
+        plt.subplot(len(dataSet_val),1,i)
+        plt.plot(dataSet_val[k])
+        plt.title(k)
+    plt.show()
+def main():
+    graphique(dataSet)
+if __name__ == "__main__":
+    main()
