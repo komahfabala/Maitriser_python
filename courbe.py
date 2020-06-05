497df0e06bc0cab23cb2e4bbd7bfb2fcb0f80930
komahfabala <komahfabala@gmail.com>
2020-06-05 15:17:46 +0200

    traçage de plusieurs courbes sur le meme graphique

--- courbe.py
+++ courbe.py
@@ -3,9 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dataSet = {f"experinece{i}" : random.randn(100) for i in range(5)} #nestead dict
-liste_elmt = [list(random.randn(10)) for i in range(5)]
-print(liste_elmt[1:3])
-
 def graphique(dataSet_val) :
     plt.figure(figsize=(12,8))
     for k,i in zip(dataSet_val.keys(), range(1,len(dataSet_val)+1)):
@@ -13,8 +10,9 @@ def graphique(dataSet_val) :
         plt.plot(dataSet_val[k])
         plt.title(k)
     plt.show()
+    plt.savefig('plusieurs_courbe.png')
 
 def main():
     graphique(dataSet)
 if __name__ == "__main__":
-    main()
+     main()
